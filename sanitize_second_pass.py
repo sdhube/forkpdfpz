bab50e3e14ecfac5072ceb7f0f9a233a5d4226d3
itd <itd220222@gmail.com>
2026-07-28 13:56:50 +0300

    delete dead code

--- sanitize_second_pass.py
+++
@@ -1,3 +0,0 @@
-# sanitize_second_pass.py
-# Functions moved to pdf_sanitize.py. Keep this file as a placeholder to avoid import errors in older versions.
-# Use the implementations in pdf_sanitize.py instead.
